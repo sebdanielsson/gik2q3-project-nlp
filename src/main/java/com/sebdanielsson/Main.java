17b51e6240f27e5a55544187b78382c397553be7
Sebastian <sebastian.danielsson@proton.me>
2024-03-08 13:25:56 +0100

    Add hadoop deps, add dir path

--- src/main/java/com/sebdanielsson/Main.java
+++ src/main/java/com/sebdanielsson/Main.java
@@ -1,121 +1,97 @@
 package com.sebdanielsson;
 
-import edu.stanford.nlp.coref.data.CorefChain;
-import edu.stanford.nlp.ling.*;
-import edu.stanford.nlp.ie.util.*;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.fs.RemoteIterator;
+import org.apache.hadoop.fs.LocatedFileStatus;
 import edu.stanford.nlp.pipeline.*;
-import edu.stanford.nlp.semgraph.*;
-import edu.stanford.nlp.trees.*;
+import edu.stanford.nlp.ling.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.URI;
 import java.util.*;
 
 public class Main {
-
-    public static String text = "Joe Smith was born in California. " +
-            "In 2017, he went to Paris, France in the summer. " +
-            "His flight left at 3:00pm on July 10th, 2017. " +
-            "After eating some escargot for the first time, Joe said, \"That was delicious!\" " +
-            "He sent a postcard to his sister Jane Smith. " +
-            "After hearing about Joe's trip, Jane decided she might go to France one day.";
-
     public static void main(String[] args) {
-        // set up pipeline properties
-        Properties props = new Properties();
-        // set the list of annotators to run
-        props.setProperty("annotators", "tokenize,pos,lemma,ner,parse,depparse,coref,kbp,quote");
-        // set a property for an annotator, in this case the coref annotator is being
-        // set to use the neural algorithm
-        props.setProperty("coref.algorithm", "neural");
-        // build pipeline
-        StanfordCoreNLP pipeline = new StanfordCoreNLP(props);
-        // create a document object
-        CoreDocument document = new CoreDocument(text);
-        // annnotate the document
-        pipeline.annotate(document);
-        // examples
+        // Check if the user provided the directory path
+        if (args.length == 0) {
+            System.out.println("The first argument should be the directory path.");
+            return;
+        }
 
-        // 10th token of the document
-        CoreLabel token = document.tokens().get(10);
-        System.out.println("Example: token");
-        System.out.println(token);
-        System.out.println();
+        String directoryPath = args[0];
 
-        // text of the first sentence
-        String sentenceText = document.sentences().get(0).text();
-        System.out.println("Example: sentence");
-        System.out.println(sentenceText);
-        System.out.println();
+        // Set up the Hadoop configuration and file system object
+        Configuration conf = new Configuration();
+        FileSystem fs = null;
+        try {
+            fs = FileSystem.get(URI.create(directoryPath), conf);
+            RemoteIterator<LocatedFileStatus> fileStatusListIterator = fs.listFiles(new Path(directoryPath), false);
+            while (fileStatusListIterator.hasNext()) {
+                LocatedFileStatus fileStatus = fileStatusListIterator.next();
+                // Check if file is a .txt file
+                if (fileStatus.getPath().getName().endsWith(".txt")) {
+                    BufferedReader br = new BufferedReader(new InputStreamReader(fs.open(fileStatus.getPath())));
+                    StringBuilder sb = new StringBuilder();
+                    String line;
+                    while ((line = br.readLine()) != null) {
+                        sb.append(line).append(" ");
+                    }
+                    br.close();
 
-        // second sentence
-        CoreSentence sentence = document.sentences().get(1);
-
-        // list of the part-of-speech tags for the second sentence
-        List<String> posTags = sentence.posTags();
-        System.out.println("Example: pos tags");
-        System.out.println(posTags);
-        System.out.println();
-
-        // list of the ner tags for the second sentence
-        List<String> nerTags = sentence.nerTags();
-        System.out.println("Example: ner tags");
-        System.out.println(nerTags);
-        System.out.println();
-
-        // constituency parse for the second sentence
-        Tree constituencyParse = sentence.constituencyParse();
-        System.out.println("Example: constituency parse");
-        System.out.println(constituencyParse);
-        System.out.println();
-
-        // dependency parse for the second sentence
-        SemanticGraph dependencyParse = sentence.dependencyParse();
-        System.out.println("Example: dependency parse");
-        System.out.println(dependencyParse);
-        System.out.println();
-
-        // kbp relations found in fifth sentence
-        List<RelationTriple> relations = document.sentences().get(4).relations();
-        System.out.println("Example: relation");
-        System.out.println(relations.get(0));
-        System.out.println();
-
-        // entity mentions in the second sentence
-        List<CoreEntityMention> entityMentions = sentence.entityMentions();
-        System.out.println("Example: entity mentions");
-        System.out.println(entityMentions);
-        System.out.println();
-
-        // coreference between entity mentions
-        CoreEntityMention originalEntityMention = document.sentences().get(3).entityMentions().get(1);
-        System.out.println("Example: original entity mention");
-        System.out.println(originalEntityMention);
-        System.out.println("Example: canonical entity mention");
-        System.out.println(originalEntityMention.canonicalEntityMention().get());
-        System.out.println();
-
-        // get document wide coref info
-        Map<Integer, CorefChain> corefChains = document.corefChains();
-        System.out.println("Example: coref chains for document");
-        System.out.println(corefChains);
-        System.out.println();
-
-        // get quotes in document
-        List<CoreQuote> quotes = document.quotes();
-        CoreQuote quote = quotes.get(0);
-        System.out.println("Example: quote");
-        System.out.println(quote);
-        System.out.println();
-
-        // original speaker of quote
-        // note that quote.speaker() returns an Optional
-        System.out.println("Example: original speaker of quote");
-        System.out.println(quote.speaker().get());
-        System.out.println();
-
-        // canonical speaker of quote
-        System.out.println("Example: canonical speaker of quote");
-        System.out.println(quote.canonicalSpeaker().get());
-        System.out.println();
-
+                    // Process the text content
+                    String textContent = sb.toString();
+                    processText(textContent);
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if (fs != null) {
+                try {
+                    fs.close();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }
     }
 
+    public static void processText(String text) {
+        // Set up pipeline properties
+        Properties props = new Properties();
+        props.setProperty("annotators", "tokenize,ssplit,pos,lemma");
+
+        // Initialize the pipeline with the properties
+        StanfordCoreNLP pipeline = new StanfordCoreNLP(props);
+
+        // Create a document object
+        CoreDocument document = new CoreDocument(text);
+
+        // Annotate the document
+        pipeline.annotate(document);
+
+        // StringBuilder to collect the processed text
+        StringBuilder processedText = new StringBuilder();
+
+        // Iterate over the sentences in the document
+        for (CoreSentence sentence : document.sentences()) {
+            // Iterate over the tokens in the sentence
+            for (CoreLabel token : sentence.tokens()) {
+                String lemma = token.lemma(); // Lemmatized word
+                // Only append words that are not punctuation (you can enhance this check as
+                // needed)
+                if (!lemma.matches("\\p{Punct}")) {
+                    processedText.append(lemma).append(" ");
+                }
+            }
+        }
+
+        // Trim the string to remove the last space
+        String finalText = processedText.toString().trim().toLowerCase();
+
+        // Output the processed text
+        System.out.println(finalText);
+    }
 }
